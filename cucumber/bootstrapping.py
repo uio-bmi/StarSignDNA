ef91f75483403d984dac6b7c1deaa02042172bfa
Knut Rand <knutdrand@gmail.com>
2023-06-09 10:07:34 +0200

    Added vcf mutation counting

--- cucumber/bootstrapping.py
+++ cucumber/bootstrapping.py
@@ -3,14 +3,15 @@ import numpy as np
 from cucumber import refit
 
 
-def bootstrap_sample(M):
+def bootstrap_sample(M, rng):
     assert (M.ndim == 2 and  M.shape[0] == 1)  or M.ndim ==1
-    ps = M/M.sum
-    rng = np.random.default_rng()
-    sampled = rng.multinomial(M.sum(), ps)
+    total_count = M.sum()
+    ps = M / total_count
+    sampled = rng.multinomial(total_count, ps)
     return np.broadcast_to(sampled, M.shape)
 
 
 def bootstrap(M, S, O,  n_bootstraps = 100, lambd=0):
-    exposures = [refit(bootstrap_sample(M), S, O, lambd=lambd) for _ in range(n_bootstraps)]
+    rng = np.random.default_rng()
+    exposures = [refit(bootstrap_sample(M, rng), S, O, lambd=lambd) for _ in range(n_bootstraps)]
     return np.mean(exposures, axis=0), np.std(exposures, axis=0)
