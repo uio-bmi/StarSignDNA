774b30c938fd40ffa0ec02085f7fa9eca37b81ea
cdbope <christianbope@aims.ac.za>
2023-09-08 16:54:04 +0200

    test the package in bmi

--- cucumber/cli.py
+++ cucumber/cli.py
@@ -163,7 +163,7 @@ def plot_profile(data):
 
 def refit(matrix_file: str, signature_file: str, output_file_exposure: str,
           opportunity_file: str = None, ref_genome: str = None,
-          data_type: DataType = DataType.exome, n_bootstraps: int = 25, numeric_chromosomes: bool = False,
+          data_type: DataType = DataType.exome, n_bootstraps: int = 2, numeric_chromosomes: bool = False,
           genotyped: bool = True, output_folder: str = 'output/'):
     '''
     Parameters
@@ -200,7 +200,7 @@ def refit(matrix_file: str, signature_file: str, output_file_exposure: str,
         E.drop(columns=E.columns[0], axis=1, inplace=True)
         plot = single_plot(E)
         E.to_csv(output_file_exposure, index=True, header=True, sep='\t')
-        plot.savefig(f"{output_folder}/exposure_tcga_brca_2_70723.png", dpi=600)
+        plot.savefig(f"{output_folder}/exposure_matrix.png", dpi=600)
 
     else:
         E = _refit(M, S, O, lambd=lambd)
