102307468af16c517a75b1056bcb6ecfd05eb6a0
BumpVersion Action <bumpversion@github-actions>
2024-05-29 09:10:31 +0000

    Bump version: 0.0.10 → 0.0.11

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/uio-bmi/starsign',
-    version='0.0.10',
+    version='0.0.11',
     zip_safe=False,
 )
