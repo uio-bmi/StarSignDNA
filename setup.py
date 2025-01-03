416abf53ad663e261d4db958836574e0aa7d0e4a
BumpVersion Action <bumpversion@github-actions>
2025-01-03 07:17:05 +0000

    Bump version: 1.0.3 → 1.0.4

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/uio-bmi/starsigndna',
-    version='1.0.3',
+    version='1.0.4',
     zip_safe=False,
 )
--- starsigndna/__init__.py
+++ starsigndna/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Christian Bope"""
 __email__ = 'chrisbop@uio.no'
-__version__ = '1.0.3'
+__version__ = '1.0.4'
 
 
 from .refit import refit
