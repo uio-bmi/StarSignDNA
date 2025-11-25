be6317c906951c632033b92809f6492f2330a49b
cdbope <christianbope@aims.ac.za>
2025-11-25 09:26:11 +0100

    Bump version to 1.0.5 for new release

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/uio-bmi/starsigndna',
-    version='1.0.1',
+    version='1.0.5',
     zip_safe=False,
 )
